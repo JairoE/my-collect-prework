545afc56656a9d4efb08d56a6620f598220bc8bd
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-20 03:16:10 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(array)
   i=0
   array2=[]
   while i < array.length
-    yield array[i]
-    array2[i]=array[i]
+    novo = yield array[i]
+    array2[i]= novo
     i=i+1
   end
   array2
